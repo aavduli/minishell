8fc4d0d7d1475a767090e1e4d73f687c73b08dc8
aavduli <aavduli@42lausanne.ch>
2024-06-11 10:28:51 +0200

    little update

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,13 +6,14 @@
 /*   By: aavduli <aavduli@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/23 15:57:39 by avdylavduli       #+#    #+#             */
-/*   Updated: 2024/06/05 11:06:09 by aavduli          ###   ########.fr       */
+/*   Updated: 2024/06/11 10:23:00 by aavduli          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
+# include "libft/libft.h"
 # include <stdio.h>
 # include <unistd.h>
 # include <stdlib.h>
