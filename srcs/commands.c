3bbc0cd3ff2e39c2dae6956fdc8082924a42078d
Avdu <avduli.avdul@gmail.com>
2024-07-08 12:05:51 +0200

    finish cmd exec

--- srcs/commands.c
+++ srcs/commands.c
@@ -6,11 +6,34 @@
 /*   By: aavduli <aavduli@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/18 16:25:05 by aavduli           #+#    #+#             */
-/*   Updated: 2024/07/08 10:29:36 by aavduli          ###   ########.fr       */
+/*   Updated: 2024/07/08 12:04:42 by aavduli          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
+
+void	ft_echo(t_data *data)
+{
+	int	i;
+	int	n_flag;
+
+	i = 1;
+	n_flag = 0;
+	if (data->str[1] && ft_strncmp(data->str[1], "-n", 2) == 0)
+	{
+		n_flag = 1;
+		i++;
+	}
+	while (data->str[i])
+	{
+		ft_putstr_fd(data->str[i], 1);
+		if (data->str[i + 1])
+			ft_putstr_fd(" ", 1);
+		i++;
+	}
+	if (!n_flag)
+		ft_putstr_fd("\n", 1);
+}
 
 void	ft_pwd(t_data *data)
 {
@@ -33,16 +56,16 @@ void	ft_cmd(t_data *data)
 {
 	if (ft_strncmp(data->str[0], "echo", 4) == 0)
 		ft_echo(data);
-	// else if (ft_strcmp(data.cmd->cmd, "cd") == 0)
-	// 	ft_cd(data); //TODO
-	if (ft_strncmp(data->str[0], "pwd", 3) == 0)
+	else if (ft_strcmp(data.cmd->cmd, "cd") == 0)
+		ft_cd(data);
+	else if (ft_strncmp(data->str[0], "pwd", 3) == 0)
 		ft_pwd(data);
-	// else if (ft_strcmp(data.cmd->cmd, "export") == 0)
-	// 	ft_export(data); //TODO
-	// else if (ft_strcmp(data.cmd->cmd, "unset") == 0)
-	// 	ft_unset(data); //TODO
-	// else if (ft_strcmp(data.cmd->cmd, "env") == 0)
-	// 	ft_env(data); //TODO
+	else if (ft_strcmp(data.cmd->cmd, "export") == 0)
+		ft_export(data);
+	else if (ft_strcmp(data.cmd->cmd, "unset") == 0)
+		ft_unset(data);
+	else if (ft_strcmp(data.cmd->cmd, "env") == 0)
+		ft_env(data);
 	else if (ft_strncmp(data->str[0], "grep", 4) == 0)
 		ft_execute(data);
 	else
---
+++ srcs/env_cmd.c
@@ -0,0 +1,80 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   env_cmd.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aavduli <aavduli@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/08 10:50:31 by aavduli           #+#    #+#             */
+/*   Updated: 2024/07/08 12:04:27 by aavduli          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/minishell.h"
+
+void	ft_export(t_data *data)
+{
+	int	i;
+
+	i = 0;
+	while (data->env[i])
+	{
+		if (ft_strncmp(data->env[i], data->str[1],
+				ft_strlen(data->str[1])) == 0)
+		{
+			data->env[i] = ft_strjoin(data->str[1], data->str[2]);
+			break ;
+		}
+		else if (data->env[i + 1] == NULL)
+		{
+			data->env[i + 1] = ft_strjoin(data->str[1], data->str[2]);
+			break ;
+		}
+		i++;
+	}
+}
+
+void	ft_unset(t_data *data)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (data->env[i])
+	{
+		if (ft_strncmp(data->env[i], data->str[1],
+				ft_strlen(data->str[1])) == 0)
+		{
+			free(data->env[i]);
+			break ;
+		}
+		j = i;
+		while (env[i])
+		{
+			data->env[j] = data->env[j + 1];
+			j++;
+		}
+		i++;
+	}
+}
+
+void	ft_cd(t_data *data)
+{
+	int	i;
+
+	i = 0;
+	while (data->env[i])
+	{
+		if (ft_strncmp(data->env[i] == "PWD=", 4,) == 0)
+		{
+			if (chdir(data->str[1]) == -1)
+				ft_putstr_fd("minishell: cd: no such file or directory\n", 1);
+			else
+			{
+				data->env[i] = ft_strjoin("PWD=", data->str[1]);
+				break ;
+			}
+		}
+		i++;
+	}
+}
