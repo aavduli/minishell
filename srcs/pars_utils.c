706035154881fdc496d7052f29d464a0c8f866c2
Floriano Albertini <floriano.webdev@gmail.com>
2024-07-15 14:31:38 +0200

    search VAR in ENV and change node

--- srcs/pars_utils.c
+++ srcs/pars_utils.c
@@ -6,27 +6,38 @@
 /*   By: falberti <falberti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/19 10:22:58 by falberti          #+#    #+#             */
-/*   Updated: 2024/07/15 14:27:38 by falberti         ###   ########.fr       */
+/*   Updated: 2024/07/15 14:30:53 by falberti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
 #include "../includes/minishell.h"
 
-static void	check_var_str(t_data *data)
+static void	check_var_str(t_data *data, t_cmd *cmd)
 {
-	(void)data;
-	return ;
-}
+	int		i;
+	char	*cpy;
+	size_t	var_len;
 
-int	get_nb_strs(char **strs)
-{
-	int	nb;
-
-	nb = 0;
-	while (strs[nb])
-		nb++;
-	return (nb);
+	if (cmd->str[0] == '$')
+		cpy = cmd->str + 1;
+	else
+		cpy = cmd->str;
+	var_len = ft_strlen(cpy);
+	i = 0;
+	while (data->env[i])
+	{
+		if (ft_strncmp(data->env[i], cpy, var_len) == 0
+			&& data->env[i][var_len] == '=')
+		{
+			free(cmd->str);
+			cmd->str = ft_strdup(data->env[i] + var_len + 1);
+			return ;
+		}
+		i++;
+	}
+	free(cmd->str);
+	cmd->str = NULL;
 }
 
 char	**ft_cpy_env(char **strs)
@@ -59,7 +70,7 @@ void	check_update_type(t_data *data)
 		current->type = determine_type(current->str);
 		if (current->type == 2)
 		{
-			check_var_str(data);
+			check_var_str(data, current);
 		}
 		current = current->next;
 	}
