a7917bf376ddac215ce2c43f0e9177b37280e1b8
Avdu <avduli.avdul@gmail.com>
2024-07-23 16:23:44 +0200

    little correction

--- srcs/lst_utils.c
+++ srcs/lst_utils.c
@@ -6,7 +6,7 @@
 /*   By: aavduli <aavduli@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/10 14:49:15 by aavduli           #+#    #+#             */
-/*   Updated: 2024/07/23 15:33:59 by aavduli          ###   ########.fr       */
+/*   Updated: 2024/07/23 16:22:31 by aavduli          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int	lst_cmd_size(t_data *data)
 
 	current = data->cmd;
 	size = 0;
-	while (current && ((current->type >= 0 && current->type <= 2) || current->type == 7))
+	while (current && (current->type >= 0 && current->type <= 2))
 	{
 		size++;
 		current = current->next;
