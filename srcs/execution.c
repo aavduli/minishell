ad3150ab8a1e2497ee6f3acbd3b4b6e557a38fe4
Floriano Albertini <floriano.webdev@gmail.com>
2024-07-24 13:40:36 +0200

    test

--- srcs/execution.c
+++ srcs/execution.c
@@ -6,7 +6,7 @@
 /*   By: falberti <falberti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/16 14:33:20 by aavduli           #+#    #+#             */
-/*   Updated: 2024/07/24 12:49:38 by falberti         ###   ########.fr       */
+/*   Updated: 2024/07/24 13:36:02 by falberti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,32 +26,59 @@ void	ft_launch(t_data *data, char **cmd)
 		return ;
 }
 
-void	ft_read_lst(t_data *d)
+// void	ft_read_lst(t_data *d)
+// {
+// 	char	**cmd;
+
+// 	cmd = NULL;
+// 	while (d->cmd)
+// 	{
+// 		if ((d->cmd->type >= 0 && d->cmd->type <= 2)
+// 			|| (d->cmd->type >= 7 && d->cmd->type <= 8))
+// 		{
+// 			cmd = creat_tab(d, cmd);
+// 			while (d->cmd && ((d->cmd->type >= 0 && d->cmd->type <= 2)
+// 					|| (d->cmd->type >= 7 && d->cmd->type <= 8)))
+// 				d->cmd = d->cmd->next;
+// 		}
+// 		if (d->cmd && ((d->cmd->type >= 3 && d->cmd->type <= 6)
+// 				|| (d->cmd->type >= 9 && d->cmd->type <= 11)))
+// 		{
+// 			check_redir(d, cmd);
+// 			while (d->cmd && ((d->cmd->type >= 3 && d->cmd->type <= 6)
+// 					|| (d->cmd->type >= 9 && d->cmd->type <= 11)))
+// 				d->cmd = d->cmd->next;
+// 		}
+// 		else if (cmd)
+// 			ft_launch(d, cmd);
+// 		if (d->cmd == NULL)
+// 			return ;
+// 	}
+// }
+
+void	ft_read_lst(t_data *data)
 {
 	char	**cmd;
 
 	cmd = NULL;
-	while (d->cmd)
+	while (data->cmd)
 	{
-		if ((d->cmd->type >= 0 && d->cmd->type <= 2)
-			|| (d->cmd->type >= 7 && d->cmd->type <= 8))
+		if (data->cmd->type >= 0 && data->cmd->type <= 2)
 		{
-			cmd = creat_tab(d, cmd);
-			while (d->cmd && ((d->cmd->type >= 0 && d->cmd->type <= 2)
-					|| (d->cmd->type >= 7 && d->cmd->type <= 8)))
-				d->cmd = d->cmd->next;
+			cmd = creat_tab(data, cmd);
+			printf("cmd2 = %s\n", cmd[1]);
+			while (data->cmd && data->cmd->type >= 0 && data->cmd->type <= 2)
+				data->cmd = data->cmd->next;
 		}
-		if (d->cmd && ((d->cmd->type >= 3 && d->cmd->type <= 6)
-				|| (d->cmd->type >= 9 && d->cmd->type <= 11)))
+		if (data->cmd && (data->cmd->type >= 3 && data->cmd->type <= 11))
 		{
-			check_redir(d, cmd);
-			while (d->cmd && ((d->cmd->type >= 3 && d->cmd->type <= 6)
-					|| (d->cmd->type >= 9 && d->cmd->type <= 11)))
-				d->cmd = d->cmd->next;
+			check_redir(data, cmd);
+			while (data->cmd && (data->cmd->type >= 3 && data->cmd->type <= 11))
+				data->cmd = data->cmd->next;
 		}
 		else if (cmd)
-			ft_launch(d, cmd);
-		if (d->cmd == NULL)
+			ft_launch(data, cmd);
+		if (data->cmd == NULL)
 			return ;
 	}
 }
